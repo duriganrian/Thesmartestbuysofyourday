12c3fe96ba74b8b0e9656480e874100d01d6ddce
Rian siqueira durigan <durigansrian@gmail.com>
2025-02-18 13:46:18 -0300

    corrigindo erro de cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,7 +5,12 @@ import { router } from './router'; //
 const server = express();
 
 server.use(express.json());
-server.use(cors());
+server.use(cors({
+    origin: "https://thesmartestbuysofyourday.com", // Permite requisições apenas deste domínio
+    methods: ["GET", "POST", "PUT", "DELETE"], // Permite esses métodos
+    allowedHeaders: ["Content-Type", "Authorization"] // Permite esses cabeçalhos
+}));
+
 server.use(router);
 
 export { server };
